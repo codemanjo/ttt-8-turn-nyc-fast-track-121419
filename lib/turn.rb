915a783abad0eb2fb6aa108696e90bf7e061ea98
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-03-06 23:15:16 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+ board= [""," "," "," "," "," "," "," "]
+puts "Welcome to Tic Tac Toe"
+
+def display_board (board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+display_board(board)
+
+def turn(board)
+  puts "Please enter 1-9"
+end
+
+turn(board)
+
+def valid_move?(board,index)
+end
